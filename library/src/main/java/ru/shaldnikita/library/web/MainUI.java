2e795ccac84b6ec44a81c8b630b0ee42178d124b
Nikita Shaldenkov <nikita.shaldenkov@bostongene.com>
2018-09-10 09:20:31 +0300

    Добавил library в трэвис и докер хаб.

---
+++ library/src/main/java/ru/shaldnikita/library/web/MainUI.java
@@ -0,0 +1,15 @@
+package ru.shaldnikita.library.web;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+/**
+ * @author Nikita Shaldenkov <nikita.shaldenkov@bostongene.com>
+ */
+@Route("")
+public class MainUI extends Div {
+
+    public MainUI() {
+        setText("ASD");
+    }
+}
